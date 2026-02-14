40f23f1176f8653fc432bb5c43a3d54fdccf36e4
VoidMaster23 <edsonshivuri@gmail.com>
2026-02-14 20:38:03 +0100

    feat: implemented utils

--- src/include/utils.h
+++ src/include/utils.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <QString>
+#include <QWidget>
 
 namespace StringUtils {
 
-    QString format_for_display(const QString &string);
+QString format_for_display(const QString &string);
 
+}
+
+namespace WidgetUtils {
+void center_in_window(QWidget *widget);
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,10 @@
 #include "utils.h"
-#include <QFont>
 #include <QString>
 #include <QStringList>
+#include <QWidget>
+
+#include <QGuiApplication>
+#include <QScreen>
 
 QString StringUtils::format_for_display(const QString &string) {
   QStringList words_list{string.split('_', Qt::SkipEmptyParts)};
@@ -11,4 +14,19 @@ QString StringUtils::format_for_display(const QString &string) {
   }
 
   return words_list.join(' ');
+}
+
+void WidgetUtils::center_in_window(QWidget *widget) {
+  widget->ensurePolished();
+  widget->adjustSize();
+
+  // Use the content rectangle to avoid title-bar confusion
+  QRect parentArea = widget->parentWidget()->rect();
+  QRect childRect = widget->rect();
+
+  // The math that never fails if dimensions are correct
+  int x = (parentArea.width() - childRect.width()) / 2;
+  int y = (parentArea.height() - childRect.height()) / 2;
+
+  widget->move(x, y);
 }
