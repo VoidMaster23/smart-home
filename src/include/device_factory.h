52a8adc3f9d6faf9864baeb86b9adf2d00965ddf
VoidMaster23 <edsonshivuri@gmail.com>
2026-03-14 16:36:07 +0100

    refactor: simplified device factory

--- src/include/device_factory.h
+++ src/include/device_factory.h
@@ -1,18 +1,11 @@
 #pragma once
 
 #include "smart_device.h"
-#include "smart_light.h"
-#include <QObject>
-#include <QString>
 #include <QJsonObject>
-
-#include <optional>
 
 #include <QPointer>
 
 namespace DeviceFactory {
     QPointer<SmartDevice> create_device(QJsonObject const &data);
 
-    bool is_light(QJsonObject const &device);
-    std::optional<ColorTempRange> getColorTempRange(const QJsonObject& device);
 }
